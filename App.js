09f1af8b94d21a3c39198c7e04c001f6a5cd9abc
Sharon Shelton <100983505+sheltonsharon@users.noreply.github.com>
2022-03-24 10:31:41 +0530

    Frontend 1.1

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
 import "./App.css";
 import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
+import logo from "./trash.png";
 
 function App() {
   const [state, setState] = useState({ name: "", value: "" });
   const [data, setData] = useState([]);
   let val = data.map((x) => (
-    <h4 key={nanoid()}>
-      Name: {x.name} && value:{x.value}
-    </h4>
+    <div className="result">
+      <p
+        key={nanoid()}
+        className="data"
+        onClick={() => {
+          console.log(x);
+          setState({ name: x.name, value: x.value });
+        }}
+      >
+        Name: {x.name} && value:{x.value}
+      </p>
+      <img
+        src={logo}
+        alt="bin"
+        key={nanoid()}
+        className="bin"
+        onClick={() => deleteData(x.name)}
+      />
+    </div>
   ));
+
+  function deleteData(name) {
+    const requestOptions = {
+      method: "DELETE",
+    };
+    fetch(`http://localhost:5000/${name}`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => fetching());
+  }
 
   function sendToBackend() {
     const requestOptions = {
@@ -17,21 +43,34 @@ function App() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(state),
     };
+    if (state.name === "" || state.value === "") {
+      alert("One or two input fields are empty");
+      return;
+    }
     fetch("http://localhost:5000/create", requestOptions)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => fetching());
+    setState({ name: "", value: "" });
   }
-  useEffect(() => {
+
+  function fetching() {
+    console.log("fetching...");
     fetch("http://localhost:5000/")
       .then((response) => response.json())
       .then((data) => setData(data));
+  }
+
+  useEffect(() => {
+    fetching();
   }, []);
+
   return (
     <div className="App">
       <label>
         Name:
         <input
           type="text"
+          value={state.name}
           onChange={(e) => setState({ ...state, name: e.target.value })}
         />
       </label>
@@ -39,6 +78,7 @@ function App() {
         value:
         <input
           type="text"
+          value={state.value}
           onChange={(e) => setState({ ...state, value: e.target.value })}
         />
       </label>
@@ -52,3 +92,4 @@ function App() {
 }
 
 export default App;
+

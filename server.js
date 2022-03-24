dc479002ba07bccdc1b16e686d4330ffa4ed8a96
Sharon Shelton <100983505+sheltonsharon@users.noreply.github.com>
2022-03-24 10:33:54 +0530

    Backend 1.0

--- server.js
+++ server.js
@@ -16,21 +16,43 @@ initializeApp({
 const db = getFirestore();
 const collection = db.collection("users");
 
-app.post("/create", async (req, res) => {
-  await collection.add({
-    name: req.body.name,
-    value: req.body.value,
-  });
-  res.send({ message: "data inserted" });
-});
-
+//return all the name and value from the database
 app.get("/", async (req, res) => {
   let arr = [];
-  const data = await collection.get();
+  const data = await collection.orderBy("value").get();
   data.forEach((doc) => {
     arr.push(doc.data());
   });
   res.send(arr);
+});
+
+//if the name already exists, the value will be updated
+//otherwise a new document will be created
+app.post("/create", async (req, res) => {
+  const snapshot = await collection.where("name", "==", req.body.name).get();
+  if (snapshot.empty) {
+    await collection.add({
+      name: req.body.name,
+      value: req.body.value,
+    });
+  } else {
+    snapshot.forEach((doc) => {
+      let tmp = doc.data();
+      tmp.value = req.body.value;
+      doc.ref.update(tmp);
+    });
+  }
+  res.send({ message: "data inserted/updated" });
+});
+
+//document with the particular name will be deleted
+app.delete("/:name", async (req, res) => {
+  console.log(req.params.name);
+  const snapshot = await collection.where("name", "==", req.params.name).get();
+  snapshot.forEach((doc) => {
+    doc.ref.delete();
+  });
+  res.send({ message: "data deleted" });
 });
 
 app.listen(PORT, () => console.log(`Server starting in ${PORT}`));
